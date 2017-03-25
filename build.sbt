fdbcd038680906fcf644528498f02c9b81ffaba1
venkat <venky.kamath@gmail.com>
2017-03-25 17:25:39 +0530

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,10 @@
+name := "SparkScalaPlay"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core_2.10
+libraryDependencies += "org.apache.spark" % "spark-core_2.10" % "1.6.2"
+
+
---
+++ src/main/scala/SparkMain.scala
@@ -0,0 +1,37 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * Created by venkat kamath on 25/03/17.
+  */
+
+class NumberCrunchingJob(sc:SparkContext) {
+
+  def expMapAndFilter(): Unit = {
+    val numbersRDD = sc.parallelize(List(1,2,3))
+    val squaresRDD = numbersRDD.map(math.pow(_,2))
+    squaresRDD.collect().foreach(println)
+
+    val evenSquaresRDD = squaresRDD.filter(_%2==0)
+    evenSquaresRDD.collect().foreach(println(_))
+
+  }
+
+  def expFlatMap(): Unit = {
+    val numbersRDD = sc.parallelize(List(1,2,3))
+    val flatNumbersRDD = numbersRDD.flatMap( n => List(n, n*2, n*3))
+    flatNumbersRDD.foreach(println)
+  }
+}
+
+
+object SparkMain {
+
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf().setAppName("Spark Scala Play").setMaster("local")
+    val sc = new SparkContext(conf)
+    val numJob = new NumberCrunchingJob(sc)
+    numJob.expMapAndFilter()
+    numJob.expFlatMap()
+  }
+
+}
